e8f64cec9406185b9f12bc5ed19645b10fef80c4
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2025-01-24 22:27:48 +0700

    Create project Hibernate

---
+++ src/main/java/model/Cat.java
@@ -0,0 +1,60 @@
+package model;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Cat {
+	private Integer id;
+	private String name;
+	private Date dateOfBirth;
+	private boolean sex;
+
+//bắt buộc phải có 1 constructor rỗng
+	public Cat() {
+	}
+
+	public Cat(Integer id, String name, Date dateOfBirth, boolean sex) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.dateOfBirth = dateOfBirth;
+		this.sex = sex;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+
+}
