a3123d8b282654d9295e030781b4cfae175840b2
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2025-01-28 22:49:05 +0700

    Create Project Hibernate

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -1,7 +1,10 @@
 package test;
 
+import org.hibernate.Hibernate;
+
+import com.mysql.cj.xdevapi.SessionFactory;
+
 public class test {
 	public static void main(String[] args) {
-
-	}
+Session session = Hibernate	}
 }
