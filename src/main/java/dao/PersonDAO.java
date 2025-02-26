3b0a44166a4261c079e306ff0a4c7e76af99505a
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2025-02-26 21:49:57 +0700

    One to One relationships in Hibernate

--- src/main/java/dao/PersonDAO.java
+++ src/main/java/dao/PersonDAO.java
@@ -1,15 +1,40 @@
 package dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import model.Person;
+import util.HibernateUtil;
 
 public class PersonDAO implements DAOInterface<Person> {
 
 	@Override
 	public List<Person> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Person> list = new ArrayList<>();
+		try {
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			if (sessionFactory != null) {
+				Session session = sessionFactory.openSession();
+				Transaction tr = session.beginTransaction();
+				// thực thi câu lệnh HQL
+				String hql = "from Person p";
+				Query query = session.createQuery(hql);
+				list = query.getResultList();
+				tr.commit();
+				session.close();
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 
 	@Override
