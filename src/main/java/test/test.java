dbb68034f9d24a6d94ff358dc00336a5a9556dd0
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2025-03-08 19:07:23 +0700

    Many to One Relationships in Hibernate

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -17,15 +17,24 @@ public class test {
 			Session session = sessionFactory.openSession();
 			Transaction tr = session.beginTransaction();
 
-			Author author_1 = new Author("001", "Nguyen Nhat Anh", new Date(System.currentTimeMillis()));
-			Book b1 = new Book("B01", "Trên Đường Băng", 50000, author_1);
-			Book b2 = new Book("B02", "Tôi thấy hoa vàng trên cỏ xanh", 71000, author_1);
-			Book b3 = new Book("B03", "Tôi là Bêtô", 66000, author_1);
+//			Author author_1 = new Author("001", "Nguyen Nhat Anh", new Date(System.currentTimeMillis()));
+//			Book b1 = new Book("B01", "Trên Đường Băng", 50000, author_1);
+//			Book b2 = new Book("B02", "Tôi thấy hoa vàng trên cỏ xanh", 71000, author_1);
+//			Book b3 = new Book("B03", "Tôi là Bêtô", 66000, author_1);
+//
+//			session.saveOrUpdate(author_1);
+//			session.saveOrUpdate(b1);
+//			session.saveOrUpdate(b2);
+//			session.saveOrUpdate(b3);
+			Author author_2 = new Author("002", "Napoleon Hill", new Date(System.currentTimeMillis()));
+			Book b4 = new Book("B01", "Bí Quyết Làm Giàu Của Napolenon Hill", 64000, author_2);
+			Book b5 = new Book("B02", "Sói Già Phố Wall", 120000, author_2);
+			Book b6 = new Book("B03", "Chiến Thắng Con Quỷ Trong Bạn", 99000, author_2);
 
-			session.saveOrUpdate(author_1);
-			session.saveOrUpdate(b1);
-			session.saveOrUpdate(b2);
-			session.saveOrUpdate(b3);
+			session.saveOrUpdate(author_2);
+			session.saveOrUpdate(b4);
+			session.saveOrUpdate(b5);
+			session.saveOrUpdate(b6);
 
 			tr.commit();
 			session.close();
