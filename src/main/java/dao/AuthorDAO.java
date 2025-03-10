97ad01c1c94f3f567eb0ad379a29822f80e18a9d
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2025-03-10 11:22:09 +0700

    Many to One relationships in Hibernate

--- src/main/java/dao/AuthorDAO.java
+++ src/main/java/dao/AuthorDAO.java
@@ -1,15 +1,40 @@
 package dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Query;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import model.Author;
+import util.HibernateUtil;
 
 public class AuthorDAO implements DAOInterface<Author> {
 
 	@Override
 	public List<Author> selectAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Author> list = new ArrayList<>();
+		try {
+			SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+			if (sessionFactory != null) {
+				Session session = sessionFactory.openSession();
+				Transaction tr = session.beginTransaction();
+				String hql = "from author ";
+				Query query = session.createQuery(hql);
+				list = query.getResultList();
+				tr.commit();
+				session.close();
+			} else {
+				return null;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 
 	@Override
