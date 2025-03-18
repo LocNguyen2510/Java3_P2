b16228ad8a3559186e16d1c35bc0df7cef38093b
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2025-03-18 14:09:59 +0700

    One to Many Relationships in Hibernate

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -1,14 +1,9 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import model.Customer;
-import model.My_Order;
 import util.HibernateUtil;
 
 public class test {
@@ -33,22 +28,23 @@ public class test {
 //			session.saveOrUpdate(o2);
 //			session.saveOrUpdate(o3);
 //			session.saveOrUpdate(o4);
-			Customer c2 = new Customer("002", "Phan Manh Quynh", "cochangtraivietlencay@gmail.com");
-			My_Order o5 = new My_Order("5", "HCM", c2);
-			My_Order o6 = new My_Order("6", "HCM", c2);
-			My_Order o7 = new My_Order("7", "HCM", c2);
-			My_Order o8 = new My_Order("8", "HCM", c2);
-			List<My_Order> list = new ArrayList<>();
-			list.add(o5);
-			list.add(o6);
-			list.add(o7);
-			list.add(o8);
-			c2.setOrder(list);
-			session.saveOrUpdate(c2);
-			session.saveOrUpdate(o5);
-			session.saveOrUpdate(o6);
-			session.saveOrUpdate(o7);
-			session.saveOrUpdate(o8);
+//			Customer c2 = new Customer("002", "Phan Manh Quynh", "cochangtraivietlencay@gmail.com");
+//			My_Order o5 = new My_Order("5", "HCM", c2);
+//			My_Order o6 = new My_Order("6", "HCM", c2);
+//			My_Order o7 = new My_Order("7", "HCM", c2);
+//			My_Order o8 = new My_Order("8", "HCM", c2);
+//			List<My_Order> list = new ArrayList<>();
+//			list.add(o5);
+//			list.add(o6);
+//			list.add(o7);
+//			list.add(o8);
+//			c2.setOrder(list);
+//			session.saveOrUpdate(c2);
+//			session.saveOrUpdate(o5);
+//			session.saveOrUpdate(o6);
+//			session.saveOrUpdate(o7);
+//			session.saveOrUpdate(o8);
+			String hql = "FROM Customer c  WHERE size(c.order) > 0";
 			tr.commit();
 			session.close();
 		}
