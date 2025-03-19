d8ec5d1ed63dadc40bf1d3b5a6dab5b48f0a0e89
dinhloc2510@gmail.com <dinhloc2510@gmail.com>
2025-03-19 13:22:51 +0700

    Many to Many Relationships in Hibernate

---
+++ src/main/java/model/NhanVien.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class NhanVien {
+	@Id
+	private String id;
+	private String hoVaTen;
+	private Date ngaySinh;
+
+	public NhanVien() {
+
+	}
+
+	public NhanVien(String id, String hoVaTen, Date ngaySinh) {
+
+		this.id = id;
+		this.hoVaTen = hoVaTen;
+		this.ngaySinh = ngaySinh;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getHoVaTen() {
+		return hoVaTen;
+	}
+
+	public void setHoVaTen(String hoVaTen) {
+		this.hoVaTen = hoVaTen;
+	}
+
+	public Date getNgaySinh() {
+		return ngaySinh;
+	}
+
+	public void setNgaySinh(Date ngaySinh) {
+		this.ngaySinh = ngaySinh;
+	}
+}
